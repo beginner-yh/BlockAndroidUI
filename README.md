91c0ebd25ad1b71627b8d3f3d21ba6e4db570150
yuhaoblock <yuhao@highlight.mobi>
2023-04-14 16:33:44 +0800

    dev:首页UI框架

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # BlockAndroidUI
+Tab 目前有2个 widget 和 compose
+首页列表 展示所有UI
---
+++ app/src/main/java/com/block/androidui/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.block.androidui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var tlMain: TabLayout
+    private lateinit var vpMain: ViewPager2
+    private lateinit var mMediator: TabLayoutMediator
+    private val mTabTitle = listOf("Widget", "Compose")
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        tlMain = findViewById(R.id.tl_main)
+        vpMain = findViewById(R.id.vp_main)
+        vpMain.adapter = object : FragmentStateAdapter(supportFragmentManager, lifecycle) {
+            override fun getItemCount(): Int {
+                return mTabTitle.size
+            }
+
+            override fun createFragment(position: Int): Fragment {
+                return WidgetListFragment.newInstance("", "")
+            }
+
+        }
+        tlMain.isTabIndicatorFullWidth = false
+        mMediator = TabLayoutMediator(
+            tlMain, vpMain
+        ) { tab, position ->
+            tab.text = mTabTitle[position]
+        }
+        mMediator.attach()
+    }
+}
